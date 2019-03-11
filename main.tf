b68a0d52128b3d601bf265b9ff6b81a5c14ea60b
VEDVRAT SHARMA <vedvrat@indebted.io>
2019-03-11 12:08:37 +1100

    UPDATE

--- main.tf
+++ main.tf
@@ -1,12 +1,19 @@
+
 # Configure the AWS Provider
 provider "aws" {
-  access_key = "${var.access_key}"
-  secret_key = "${var.secret_key}"
-  version = "< 2"
+  version = "~> 1.60"
   region  = "ap-southeast-2"
 }
 
 resource "aws_instance" "example" {
   ami           = "ami-080146b44c2e28be4"
   instance_type = "t2.micro"
+}
+
+resource "aws_eip" "ip" {
+  instance = "${aws_instance.example.id}"
+}
+
+output "ip" {
+  value = "${aws_eip.ip.public_ip}"
 }
